3397305836e765dc5068a64f7de0a1f5da7cdee9
jgarrick <jgarrick@sofi.org>
2021-01-23 19:32:25 -0700

    cleanup file structure

--- SquareEmployeeInformation/SquareEmployeeInformation/Networking/EmployeesNetworkManager.swift
+++ SquareEmployeeInformation/SquareEmployeeInformation/Networking/EmployeesNetworkManager.swift
@@ -7,32 +7,6 @@
 
 import Foundation
 
-/*
- I am a sucker for dependency injection, so singletons aren't my favorite
- I am adding this just as a simple debugging tool to switch between the given
- URLs and made it a singleton to keep this feature simple
- */
-class URLManager {
-    static let shared = URLManager() ; private init() {}
-    var url = "https://s3.amazonaws.com/sq-mobile-interview/employees.json"
-    
-    enum URLResponseType {
-        case good
-        case empty
-        case malformedJSON
-    }
-    
-    func updateURLTo(type: URLResponseType) {
-        switch type {
-        case .good:
-            url = "https://s3.amazonaws.com/sq-mobile-interview/employees.json"
-        case .empty:
-            url = "https://s3.amazonaws.com/sq-mobile-interview/employees_empty.json"
-        case .malformedJSON:
-            url = "https://s3.amazonaws.com/sq-mobile-interview/employees_empty.json"
-        }
-    }
-}
 
 typealias EmployeesBackendResponse = (Result<[Employee], EmployeesNetworkError>) -> Void
 
